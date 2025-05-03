e14079f4a46206705214e73fdb1ce59868d7888f
stan <dev.stankam@gmail.com>
2025-05-04 01:54:19 +0800

    updated the playRound algorithm

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// Create two new variables named humanScore and computerScore in the global scope.
-// Initialize those variables with the value of 0. 
-
 // Step 2 
 // getComputerChoice declration
-function getComputerChoice(){
+function getComputerChoice() {
     // create a number variable and store the random value of 1 - 3
-    let randomNum = Math.floor(Math.random()*3) + 1
+    let randomNum = Math.floor(Math.random() * 3) + 1
     // seperate different cases using switch and return it
-    switch(randomNum){
+    switch (randomNum) {
         // case 1 rock
         case 1:
             return "rock"
@@ -22,7 +19,8 @@ function getComputerChoice(){
 
 
 // step 3
-function getHumanChoice(){
+// getHumanChoice declaration
+function getHumanChoice() {
     let userChoice = "";
     userChoice = prompt("Please choose \"rock\", \"paper\" or \"scissors\"");
     return userChoice;
@@ -30,30 +28,30 @@ function getHumanChoice(){
 
 
 
-function playGame(){
+function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice){
+    function playRound(humanChoice, computerChoice) {
         let lowerCaseHumanChoice = humanChoice.toLowerCase();
+        let computerWin = (lowerCaseHumanChoice === "rock" && computerChoice == "paper" ||
+        lowerCaseHumanChoice === "paper" && computerChoice == "scissors" ||
+        lowerCaseHumanChoice === "scissors" && computerChoice == "rock");
         //human lose scenerio
-        if(lowerCaseHumanChoice === "rock" && computerChoice == "paper" || 
-        lowerCaseHumanChoice === "paper" && computerChoice == "scissors" || 
-        lowerCaseHumanChoice === "scissors" && computerChoice == "rock"){
+
+        if (computerWin){
             console.log(`You lose! ${computerChoice} beats ${lowerCaseHumanChoice}`);
             computerScore++;
-        }else if(lowerCaseHumanChoice === "paper" && computerChoice == "rock" || 
-        lowerCaseHumanChoice === "scissors" && computerChoice == "paper" || 
-        lowerCaseHumanChoice === "rock" && computerChoice == "scissors"){
+        }else if(lowerCaseHumanChoice === computerChoice){
+            console.log(`Draw ! ${lowerCaseHumanChoice} and ${computerChoice}`)
+        }else{
             console.log(`You win! ${lowerCaseHumanChoice} beats ${computerChoice}`)
             humanScore++;
-        }else{
-            console.log(`Draw ! ${lowerCaseHumanChoice} and ${computerChoice}`)
         }
     }
 
-    for(let i =0 ; i<5; i++){
-        console.log(`Round${i+1}:`);
+    for (let i = 0; i < 5; i++) {
+        console.log(`Round${i + 1}:`);
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
